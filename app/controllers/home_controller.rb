2a2e5b818b96ff899b5dcf36c32443488bb4ce70
Remi Achji <achji.remi@gmail.com>
2019-03-19 15:21:47 +0100

    saving

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ShopifyApp::AuthenticatedController
     # ShopifyAPI::Session.setup(api_key: "db132a0bee18bc1e49f07ddbbcc05e55", secret: "c929af1c881e2b92847bfe6092ef8d78")
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
-    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 5, :order => "created_at DESC" })
+    @orders = ShopifyAPI::Order.find(:all)
+    @customers = ShopifyAPI::Customer.find(:all)
   end
 end
