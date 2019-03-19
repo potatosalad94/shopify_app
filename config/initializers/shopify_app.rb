d07e467ccb1b172af7fd7d7dbf23d8603dffff37
Remi Achji <achji.remi@gmail.com>
2019-03-19 13:32:43 +0100

    saving

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,7 @@ ShopifyApp.configure do |config|
   config.api_key = "db132a0bee18bc1e49f07ddbbcc05e55"
   config.secret = "c929af1c881e2b92847bfe6092ef8d78"
   config.old_secret = "<old_secret>"
-  config.scope = "read_products, read_orders" # Consult this page for more scope options:
+  config.scope = "read_products,read_orders,read_customers" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
