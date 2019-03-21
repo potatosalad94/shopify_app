0d4ebff1cdbdb6e0bdd9b610a8082e8fb937c059
Remi Achji <achji.remi@gmail.com>
2019-03-21 11:15:31 +0100

    saving

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,21 +5,15 @@ class HomeController < ShopifyApp::AuthenticatedController
     @orders = ShopifyAPI::Order.find(:all)
     @customers = ShopifyAPI::Customer.find(:all)
     @data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-          label: "My First dataset",
-          background_color: "rgba(220,220,220,0.2)",
-          border_color: "rgba(220,220,220,1)",
-          data: [65, 59, 80, 81, 56, 55, 40]
-      },
-      {
-          label: "My Second dataset",
-          background_color: "rgba(151,187,205,0.2)",
-          border_color: "rgba(151,187,205,1)",
-          data: [28, 48, 40, 19, 86, 27, 90]
-      }
-    ]
+      labels: @orders.group_by_hour_of_day(&:created_at).values.flatten.map(&:created_at).map(&:to_time),
+      datasets: [
+        {
+            label: "My First dataset",
+            background_color: "rgba(220,220,220,0.2)",
+            border_color: "rgba(220,220,220,1)",
+            data: @orders.group_by_hour_of_day(&:created_at).values.flatten.map(&:total_price).map(&:to_f)
+        }
+      ]
     }
   end
 end
